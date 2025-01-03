b3773e2dc1090198caae314070d7af03cf6ea847
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-03 14:41:26 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./assets/styles/styles.css"
+import AssemblyEndgame from "./components/AssemblyEndgame";
+function App() {
+
+  return (
+    <>
+      <AssemblyEndgame />
+    </>
+  )
+}
+
+export default App
